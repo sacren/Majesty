535d4f3cc1ebd802d0ab858e1cd29b116aee1717
Jean Sacren <sakiwit@gmail.com>
2024-05-10 19:04:21 -0600

    Build three pages out of Laravel
    This is a demo of Laravel application by building Home, About and
    Contact pages.  At the top of each page, navigational links connect to
    each of these pages.
    
    Use Laravel component for layout of the common parts of all three pages.
    
    $ sail artisan make:component --view -- layout
    
      INFO  Component created successfully.
    
    $ ls resources/views/components/
    layout.blade.php
    
    Use $slot in the layout for the unique part of each page.
    
    Additional work is needed for dynamic page title.

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Home Page</title>
+    </head>
+    <body>
+        <nav>
+            <a href='/'>Home</a>
+            <a href='/about'>About</a>
+            <a href='/contact'>Contact</a>
+        </nav>
+
+	{{ $slot }}
+    </body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,3 @@
+<x-layout>
+    <h1>Hello from Home Page</h1>
+</x-layout>
