1fbf14ca8432b567842d2f182b519d98d3588946
Jean Sacren <sakiwit@gmail.com>
2024-05-14 22:41:26 -0600

    Use navlink component for hard-coded anchor tag
    Home, About and Contact links are anchor tags hard-coded with almost
    identical attributes.  Use navlink component to reduce the repetitive
    attributes as was done before the introduction of tailwind CSS.  Apply
    navlink component to both desktop and mobile.
    
    All anchor tags were hard-coded.  This had many issues, including Home
    link always highlighted even when it was not the current page.  Fix this
    by the use of boolean value to determine if the link is the current page
    or not.
    
    Use :active="foo" to retain the boolean value of foo.  Without ':' only
    string is assigned to 'active' prop, not the boolean value.  The ':'
    prefix informs Blade that the prop assignment is a PHP expression.
    
    In Blade components, props are introduced by @props() directive and the
    directive must be the first line.
    
    @props([ 'foo' => $bar ])

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -26,9 +26,9 @@
               <div class="hidden md:block">
                 <div class="ml-10 flex items-baseline space-x-4">
                   <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                  <a href="/" class="{{request()->is('/') ? 'bg-gray-900 text-white':'text-gray-300 hover:bg-gray-700 hover:text-white'}} rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
-                  <a href="/about" class="{{request()->is('about') ? 'bg-gray-900 text-white':'text-gray-300 hover:bg-gray-700 hover:text-white'}} rounded-md px-3 py-2 text-sm font-medium">About</a>
-                  <a href="/contact" class="{{request()->is('contact') ? 'bg-gray-900 text-white':'text-gray-300 hover:bg-gray-700 hover:text-white'}} rounded-md px-3 py-2 text-sm font-medium">Contact</a>
+                  <x-navlink href="/" :active="request()->is('/')">Home</x-navlink>
+                  <x-navlink href="/about" :active="request()->is('about')">About</x-navlink>
+                  <x-navlink href="/contact" :active="request()->is('contact')">Contact</x-navlink>
                 </div>
               </div>
             </div>
@@ -76,9 +76,9 @@
         <div class="md:hidden" id="mobile-menu">
           <div class="space-y-1 px-2 pb-3 pt-2 sm:px-3">
             <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-            <a href="/" class="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium" aria-current="page">Home</a>
-            <a href="/about" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">About</a>
-            <a href="/contact" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Contact</a>
+            <x-navlink href="/" :active="request()->is('/')">Home</x-navlink>
+            <x-navlink href="/about" :active="request()->is('about')">About</x-navlink>
+            <x-navlink href="/contact" :active="request()->is('contact')">Contact</x-navlink>
           </div>
           <div class="border-t border-gray-700 pb-3 pt-4">
             <div class="flex items-center px-5">
