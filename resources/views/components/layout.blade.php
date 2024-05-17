9de8f52a76fd8b6503f274dac174a05571d91e9a
Jean Sacren <sakiwit@gmail.com>
2024-05-16 22:41:23 -0600

    Add jobs page
    The job title and the salary are the keys of an associative array.  A
    list of these arrays form an indexed array.  This indexed array becomes
    the value of the outer associative array with the key by the name of
    jobs.  The outer associative array, or the whole array, is passed in as
    data to view() function.  Nevertheless, the key of jobs is used as $jobs
    to refer to the indexed array.
    
    Build Jobs page using layout as Home page.  Loop through the indexed
    array by $jobs for the values of job title and salary.
    
    Update layout for Jobs page.  Remove About page.
    
    https://laravel.com/api/11.x/[Global_Namespace].html#function_view

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -27,7 +27,7 @@
                 <div class="ml-10 flex items-baseline space-x-4">
                   <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
                   <x-navlink href="/" :active="request()->is('/')">Home</x-navlink>
-                  <x-navlink href="/about" :active="request()->is('about')">About</x-navlink>
+                  <x-navlink href="/jobs" :active="request()->is('jobs')">Jobs</x-navlink>
                   <x-navlink href="/contact" :active="request()->is('contact')">Contact</x-navlink>
                 </div>
               </div>
@@ -77,7 +77,7 @@
           <div class="space-y-1 px-2 pb-3 pt-2 sm:px-3">
             <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
             <x-navlink href="/" :active="request()->is('/')">Home</x-navlink>
-            <x-navlink href="/about" :active="request()->is('about')">About</x-navlink>
+            <x-navlink href="/jobs" :active="request()->is('jobs')">Jobs</x-navlink>
             <x-navlink href="/contact" :active="request()->is('contact')">Contact</x-navlink>
           </div>
           <div class="border-t border-gray-700 pb-3 pt-4">
--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,23 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/about', function () {
-    return view('about');
+Route::get('/jobs', function () {
+    return view('jobs', [
+        'jobs' => [
+            [
+                'title' => 'Director',
+                'salary' => '$50,000',
+            ],
+            [
+                'title' => 'Programmer',
+                'salary' => '$10,000',
+            ],
+            [
+                'title' => 'Teacher',
+                'salary' => '$40,000',
+            ],
+        ],
+    ]);
 });
 
 Route::get('/contact', function () {
