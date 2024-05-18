600e8b9f1da69495c390e956ea253cd38f360368
Jean Sacren <sakiwit@gmail.com>
2024-05-18 12:49:55 -0600

    Add Job class to encapsulate array
    Declaring $jobs array outside a class is an example for teaching only.
    OOP provides encapsulation for local object.  In a better sense, $jobs
    array will be encapsulated in a class.  In production, $jobs will be
    populated by querying the database via a method.
    
    The use of 'use' keyword serves the same teaching purpose.  When class
    is used, 'use' keyword in this setting is not needed any more.  It's
    quite distinctive to use scope resolution operator '::' for access of
    class properties and methods.

--- routes/web.php
+++ routes/web.php
@@ -3,36 +3,44 @@
 use Illuminate\Support\Facades\Route;
 use \Illuminate\Support\Arr;
 
-$jobs = [
-    [
-        'id' => 1,
-        'title' => 'Director',
-        'salary' => '$50,000',
-    ],
-    [
-        'id' => 2,
-        'title' => 'Programmer',
-        'salary' => '$10,000',
-    ],
-    [
-        'id' => 3,
-        'title' => 'Teacher',
-        'salary' => '$40,000',
-    ],
-];
+class Job
+{
+    private static $jobs = [
+            [
+                'id' => 1,
+                'title' => 'Director',
+                'salary' => '$50,000',
+            ],
+            [
+                'id' => 2,
+                'title' => 'Programmer',
+                'salary' => '$10,000',
+            ],
+            [
+                'id' => 3,
+                'title' => 'Teacher',
+                'salary' => '$40,000',
+            ],
+        ];
+
+    public static function get(): array
+    {
+        return self::$jobs;
+    }
+}
 
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/jobs', function () use ($jobs) {
+Route::get('/jobs', function () {
     return view('jobs', [
-        'jobs' => $jobs,
+        'jobs' => Job::get(),
     ]);
 });
 
-Route::get('/jobs/{id}', function ($id) use ($jobs) {
-    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
+Route::get('/jobs/{id}', function ($id) {
+    $job = Arr::first(Job::get(), fn($job) => $job['id'] == $id);
 
     return view('job', [ 'job' => $job ]);
 });
