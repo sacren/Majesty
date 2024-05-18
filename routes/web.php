34c2fc94e47b062bdafa0832d4006a8c0ff610a6
Jean Sacren <sakiwit@gmail.com>
2024-05-17 22:31:00 -0600

    Minimize code duplication
    The jobs array is duplicate in both callback functions.  Move it to the
    parent scope to be shared.  Use 'use' keyword to make jobs array
    available to the functions.  In closures, the use keyword allows to bind
    variables from the parent scope.

--- routes/web.php
+++ routes/web.php
@@ -3,51 +3,35 @@
 use Illuminate\Support\Facades\Route;
 use \Illuminate\Support\Arr;
 
+$jobs = [
+    [
+        'id' => 1,
+        'title' => 'Director',
+        'salary' => '$50,000',
+    ],
+    [
+        'id' => 2,
+        'title' => 'Programmer',
+        'salary' => '$10,000',
+    ],
+    [
+        'id' => 3,
+        'title' => 'Teacher',
+        'salary' => '$40,000',
+    ],
+];
+
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/jobs', function () {
+Route::get('/jobs', function () use ($jobs) {
     return view('jobs', [
-        'jobs' => [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50,000',
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$10,000',
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$40,000',
-            ],
-        ],
+        'jobs' => $jobs,
     ]);
 });
 
-Route::get('/jobs/{id}', function ($id) {
-    $jobs = [
-        [
-            'id' => 1,
-            'title' => 'Director',
-            'salary' => '$50,000',
-        ],
-        [
-            'id' => 2,
-            'title' => 'Programmer',
-            'salary' => '$10,000',
-        ],
-        [
-            'id' => 3,
-            'title' => 'Teacher',
-            'salary' => '$40,000',
-        ],
-    ];
-
+Route::get('/jobs/{id}', function ($id) use ($jobs) {
     $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
 
     return view('job', [ 'job' => $job ]);
