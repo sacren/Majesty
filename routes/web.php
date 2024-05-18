5060bcd498d5247a108508f668acec63a2b3f9d1
Jean Sacren <sakiwit@gmail.com>
2024-05-18 16:40:53 -0600

    Add Job model
    Use artisan tool to generate Job model.
    
    $ sail artisan make:model -- Job
    
    Do not use these options:
      --controller
      --factory
      --migration
    
    $ ls app/Models/Job.php
    app/Models/Job.php
    
    Reconstruct the new Job class body with the old Job body.
    
    Apply App\Models\Job namespace for access to new Job.
    
    The old Job class is obsolete and clean it up.

--- routes/web.php
+++ routes/web.php
@@ -2,32 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Arr;
-
-class Job
-{
-    private static $jobs = [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50,000',
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$10,000',
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$40,000',
-            ],
-        ];
-
-    public static function get(): array
-    {
-        return self::$jobs;
-    }
-}
+use App\Models\Job;
 
 Route::get('/', function () {
     return view('home');
