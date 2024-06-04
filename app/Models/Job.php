138e38c7588f415b596e4a488c26d46bc7ae352c
Jean Sacren <sakiwit@gmail.com>
2024-06-03 18:35:19 -0600

    Add employer() for Job to Employer relationship
    The employer() method builds a relationship between Job and Employer
    models.  Job object belongs to Employer object and Employer object could
    have many Job objects.  The method is used as if it were Job property.
    
    > $job = Job::first();
    > $job->employer;
    = App\Models\Employer {#5839
        id: 6,
        name: "Rath, Crist and Romaguera",
        created_at: "2024-06-02 20:37:55",
        updated_at: "2024-06-02 20:37:55",
      }
    > $job;
    = App\Models\Job {#5088
        id: 1,
        employer_id: 6,
        title: "Broadcast News Analyst",
        salary: "$103,000.00",
        created_at: "2024-06-03 00:34:29",
        updated_at: "2024-06-03 00:34:29",
        employer: App\Models\Employer {#5839
          id: 6,
          name: "Rath, Crist and Romaguera",
          created_at: "2024-06-02 20:37:55",
          updated_at: "2024-06-02 20:37:55",
        },
      }
    
    Employer::class is a string of fully qualified Employer class name.
    
    Add return type of Relation to the method.  Add method documentation.

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Relation;
 
 class Job extends Model
 {
@@ -21,5 +22,15 @@ class Job extends Model
         }
 
         return $job;
+    }
+
+    /**
+     * Build a relationship from Job to Employer.
+     *
+     * @return Relation.
+     */
+    public function employer(): Relation
+    {
+        return $this->belongsTo(Employer::class);
     }
 }
