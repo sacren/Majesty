2c57613a50223a1fceb00738c4ad19e529a5d8d9
Jean Sacren <sakiwit@gmail.com>
2024-06-03 20:57:51 -0600

    Add jobs() for Employer to Job relationship
    The jobs() method builds the relationship from Employer model to Job
    model.  Employer object could have many Job objects.  This maintains
    one-to-many relationship.  The method is used as if it were Employer
    property.
    
    > $employer = Employer::find(6);
    = App\Models\Employer {#6078
        id: 6,
        name: "Rath, Crist and Romaguera",
        created_at: "2024-06-02 20:37:55",
        updated_at: "2024-06-02 20:37:55",
      }
    
    > $employer->jobs;
    = Illuminate\Database\Eloquent\Collection {#6081
        all: [
          App\Models\Job {#5088
            id: 1,
            employer_id: 6,
            title: "Broadcast News Analyst",
            salary: "$103,000.00",
            created_at: "2024-06-03 00:34:29",
            updated_at: "2024-06-03 00:34:29",
          },
          App\Models\Job {#6099
            id: 2,
            employer_id: 6,
            title: "Embossing Machine Operator",
            salary: "$194,000.00",
            created_at: "2024-06-03 00:34:29",
            updated_at: "2024-06-03 00:34:29",
          },
        ],
      }
    
    > $employer;
    = App\Models\Employer {#6078
        id: 6,
        name: "Rath, Crist and Romaguera",
        created_at: "2024-06-02 20:37:55",
        updated_at: "2024-06-02 20:37:55",
        jobs: Illuminate\Database\Eloquent\Collection {#6081
          all: [
            App\Models\Job {#5088
              id: 1,
              employer_id: 6,
              title: "Broadcast News Analyst",
              salary: "$103,000.00",
              created_at: "2024-06-03 00:34:29",
              updated_at: "2024-06-03 00:34:29",
            },
            App\Models\Job {#6099
              id: 2,
              employer_id: 6,
              title: "Embossing Machine Operator",
              salary: "$194,000.00",
              created_at: "2024-06-03 00:34:29",
              updated_at: "2024-06-03 00:34:29",
            },
          ],
        },
      }
    
    Job::class is a string of fully qualified Job class name.
    
    Add return type of Relation to the method.  Add method documentation.

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Relation;
 
 class Employer extends Model
 {
     use HasFactory;
+
+    /**
+     * Build a relationship from Employer to Job.
+     *
+     * @return Relation.
+     */
+    public function jobs(): Relation
+    {
+        return $this->HasMany(Job::class);
+    }
 }
