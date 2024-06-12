cb099d5fe545c407b4c9d049bc8adfc9e66fdc67
Jean Sacren <sakiwit@gmail.com>
2024-06-12 17:54:21 -0600

    Add many-to-many relationships between models
    One job can have many tags and one tag can be assigned to many jobs.
    Between Job and Tag, build many-to-many relationships.  Use intermediary
    pivot table to regulate this relationship.
    
    In pivot table the foreign key for Job is job_listing_id rather than the
    default job_id.  Use job_listing_id as respective foreignPivotKey and
    relatedPivotKey.
    
    Verify this many-to-many relationship regulated by the pivot table.
    
    > $tag->jobs;
    = Illuminate\Database\Eloquent\Collection {#5082
        all: [
          App\Models\Job {#5065
            id: 2,
            employer_id: 6,
            title: "Embossing Machine Operator",
            salary: "$194,000.00",
            created_at: "2024-06-03 00:34:29",
            updated_at: "2024-06-03 00:34:29",
            pivot: Illuminate\Database\Eloquent\Relations\Pivot {#5066
              tag_id: 3,
              job_listing_id: 2,
            },
          },
        ],
      }
    
    > $job->tags;
    = Illuminate\Database\Eloquent\Collection {#5069
        all: [
          App\Models\Tag {#6198
            id: 3,
            name: "ducimus",
            created_at: "2024-06-12 21:32:37",
            updated_at: "2024-06-12 21:32:37",
            pivot: Illuminate\Database\Eloquent\Relations\Pivot {#5064
              job_listing_id: 2,
              tag_id: 3,
            },
          },
        ],
      }
    
    > $tag->jobs()->get();
    = Illuminate\Database\Eloquent\Collection {#6084
        all: [
          App\Models\Job {#6075
            id: 2,
            employer_id: 6,
            title: "Embossing Machine Operator",
            salary: "$194,000.00",
            created_at: "2024-06-03 00:34:29",
            updated_at: "2024-06-03 00:34:29",
            pivot: Illuminate\Database\Eloquent\Relations\Pivot {#5088
              tag_id: 3,
              job_listing_id: 2,
            },
          },
          App\Models\Job {#6087
            id: 8,
            employer_id: 24,
            title: "Optometrist",
            salary: "$113,000.00",
            created_at: "2024-06-03 03:07:48",
            updated_at: "2024-06-03 03:07:48",
            pivot: Illuminate\Database\Eloquent\Relations\Pivot {#6200
              tag_id: 3,
              job_listing_id: 8,
            },
          },
        ],
      }

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Relation;
 
 class Tag extends Model
 {
     use HasFactory;
+
+    /**
+     * Build many-to-many relationship from Tag to Job.
+     *
+     * @return Relation.
+     */
+    public function jobs(): Relation
+    {
+        return $this->belongsToMany(Job::class, relatedPivotKey: 'job_listing_id');
+    }
 }
