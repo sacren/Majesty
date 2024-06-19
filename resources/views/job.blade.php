e4ed9051051f618ba33d0f534c13632c3d2c3efa
Jean Sacren <sakiwit@gmail.com>
2024-06-18 23:30:37 -0600

    Add cards for job postings
    Use Tailwind CSS and add cards for job postings.  Add color to each
    individual job.

--- resources/views/job.blade.php
+++ resources/views/job.blade.php
@@ -1,9 +1,9 @@
 <x-layout>
-    <x-slot:heading>
-        Job
-    </x-slot:heading>
-    <h2 class="font-bold text-lg">{{ $job['title'] }}</h2>
-    <p>
-        Pays {{ $job['salary'] }} a years.
-    </p>
+  <x-slot:heading>
+    Job
+  </x-slot:heading>
+  <h2 class="font-bold text-lg text-blue-500">{{ $job['title'] }}</h2>
+  <p>
+  Pays {{ $job['salary'] }} per year.
+  </p>
 </x-layout>
--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -1,14 +1,17 @@
 <x-layout>
-    <x-slot:heading>
-        Job Postings
-    </x-slot:heading>
-    <ul>
-        @foreach ($jobs as $job)
-            <li>
-                <a href="/jobs/{{ $job['id'] }}" class="hover:text-red-500 underline">
-                    <strong>{{ $job['title'] }}</strong>: Pays {{ $job['salary'] }} a years.
-                </a>
-            </li>
-        @endforeach
-    </ul>
+  <x-slot:heading>
+    Job Postings
+  </x-slot:heading>
+  <div class="space-y-4">
+    @foreach ($jobs as $job)
+    <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-red-200">
+      <div class="font-bold text-blue-500 text-sm">
+        {{ $job->employer->name }}
+      </div>
+      <div>
+        <strong>{{ $job['title'] }}</strong>: Pays {{ $job['salary'] }} a years.
+      </div>
+    </a>
+    @endforeach
+  </div>
 </x-layout>
