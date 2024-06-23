d2f2600cc47531993a131011856cdaaec236ee3c
Jean Sacren <sakiwit@gmail.com>
2024-06-23 10:48:58 -0600

    Add seed for 100 jobs
    Add this seed and 100 jobs will be created with migrate:fresh.
    
    $ sail artisan migrate:fresh --seed
    
    Verify 100 jobs have been indeed created.
    
    > Job::find(100)
    = App\Models\Job {#6601
        id: 100,
        employer_id: 100,
        title: "Freight Inspector",
        salary: "$142,000.00",
        created_at: "2024-06-23 16:50:05",
        updated_at: "2024-06-23 16:50:05",
      }
    
    > Job::find(101)
    = null

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Job;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -18,5 +19,7 @@ class DatabaseSeeder extends Seeder
             'last_name' => 'Trantow',
             'email' => 'bleffler@example.com',
         ]);
+
+        Job::factory(100)->create();
     }
 }
