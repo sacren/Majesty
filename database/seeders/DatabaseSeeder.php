2f89fcca05281cf7ff4441a36ff490eef148719f
Jean Sacren <sakiwit@gmail.com>
2024-06-23 12:14:58 -0600

    Add call to JobSeeder class
    Use JobSeeder class in lieu of Job::factory() call.  The default
    DatabaseSeeder will include the execution of JobSeeder.
    
    $ sail artisan migrate:fresh --seed
    ...
     INFO  Seeding database.
    
      Database\Seeders\JobSeeder ............................ RUNNING
      Database\Seeders\JobSeeder ...................... 1,997 ms DONE

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\Job;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,6 +18,6 @@ class DatabaseSeeder extends Seeder
             'email' => 'bleffler@example.com',
         ]);
 
-        Job::factory(100)->create();
+        $this->call(JobSeeder::class);
     }
 }
