310d96ac4570b99f9606bbe7973fab63e041ae7e
Jean Sacren <sakiwit@gmail.com>
2024-06-24 13:40:49 -0600

    Remove obsolete Job views
    The jobs and single job views have been migrated to jobs directory and
    their old counterparts are now obsolete.  Tidy up resources/views/.
    
    This concludes the migration of jobs views.  The series of commits
    dbf0f724d770..6f67c71451e4 presents the steps of views scaffolding for
    jobs of Job model.  It might be extended for other models as well.

--- resources/views/job.blade.php
+++
@@ -1,9 +0,0 @@
-<x-layout>
-  <x-slot:heading>
-    Job
-  </x-slot:heading>
-  <h2 class="font-bold text-lg text-blue-500">{{ $job['title'] }}</h2>
-  <p>
-  Pays {{ $job['salary'] }} per year.
-  </p>
-</x-layout>
