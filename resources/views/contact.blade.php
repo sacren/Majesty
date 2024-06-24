e50b1a0938ab84a3133541bf186cce35810a0022
Jean Sacren <sakiwit@gmail.com>
2024-06-24 17:22:48 -0600

    Add dynamic page title
    Use dynamic title for individual page to give a little more info about
    the page.

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,6 +1,9 @@
 <x-layout>
-    <x-slot:heading>
-        Contact Page
-    </x-slot:heading>
-    <h1>Hello from Contact Page</h1>
+  <x-slot:title>
+    Contact Page
+  </x-slot:title>
+  <x-slot:heading>
+    Contact Page
+  </x-slot:heading>
+  <h1>Hello from Contact Page</h1>
 </x-layout>
