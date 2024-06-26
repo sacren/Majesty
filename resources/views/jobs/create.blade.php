45663089bcf9cc4fab3a777a6ee72869c20c3d2c
Jean Sacren <sakiwit@gmail.com>
2024-06-26 17:25:32 -0600

    Add display of validation errors
    Iterate over $errors->all() array to display all the error messages
    through the use of Blade and let the messages pop up above the input
    fields.

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -12,6 +12,16 @@
         <h2 class="text-base font-semibold leading-7 text-gray-900">Create a new job</h2>
         <p class="mt-1 text-sm leading-6 text-gray-600">Please fill out required info.</p>
 
+        <div class="mt-5">
+          @if($errors->any())
+          <ul>
+            @foreach($errors->all() as $error)
+            <li class="text-red-500 italic">{{ $error }}</li>
+            @endforeach
+          </ul>
+          @endif
+        </div>
+
         <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
           <div class="sm:col-span-4">
             <label for="title" class="block text-sm font-medium leading-6 text-gray-900">Title</label>
