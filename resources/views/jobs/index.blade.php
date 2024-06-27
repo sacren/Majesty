358f3691b79be0a1d7f8c4fc4443cb507a9ccd84
Jean Sacren <sakiwit@gmail.com>
2024-06-27 13:29:44 -0600

    Add Edit Job button
    Use ready-made button component for the link to Edit Job page.
    
    $job is passed through the view as data object in the context of Blade.
    Use the -> operator to access its data.

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -7,12 +7,12 @@
   </x-slot:heading>
   <div class="space-y-4">
     @foreach ($jobs as $job)
-    <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-red-200">
+    <a href="/jobs/{{ $job->id }}" class="block px-4 py-6 border border-red-200">
       <div class="font-bold text-blue-500 text-sm">
         {{ $job->employer->name }}
       </div>
       <div>
-        <strong>{{ $job['title'] }}</strong>: Pays {{ $job['salary'] }} a years.
+        <strong>{{ $job->title }}</strong>: Pays {{ $job->salary }} a years.
       </div>
     </a>
     @endforeach
--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -5,8 +5,11 @@
   <x-slot:heading>
     Job
   </x-slot:heading>
-  <h2 class="font-bold text-lg text-blue-500">{{ $job['title'] }}</h2>
+  <h2 class="font-bold text-lg text-blue-500">{{ $job->title }}</h2>
   <p>
-  Pays {{ $job['salary'] }} per year.
+  Pays {{ $job->salary }} per year.
   </p>
+  <div class="mt-5">
+    <x-button href="#">Edit Job</x-button>
+  </div>
 </x-layout>
