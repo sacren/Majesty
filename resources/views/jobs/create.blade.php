820feb215eecadab9db80781936dd57f6d0e7439
Jean Sacren <sakiwit@gmail.com>
2024-06-26 21:04:08 -0600

    Add required attribute
    Add required attribute to input field to force the user input to the
    form on the client before submission is executed.

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -17,7 +17,7 @@
             <label for="title" class="block text-sm font-medium leading-6 text-gray-900">Title</label>
             <div class="mt-2">
               <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                <input type="text" name="title" id="title" class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Petroleum Technician">
+                <input type="text" name="title" id="title" class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Petroleum Technician" required>
               </div>
               <div class="mt-2">
                 @error('title')
@@ -31,7 +31,7 @@
             <label for="salary" class="block text-sm font-medium leading-6 text-gray-900">Salary</label>
             <div class="mt-2">
               <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                <input type="text" name="salary" id="salary" class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="$187,000.00">
+                <input type="text" name="salary" id="salary" class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="$187,000.00" required>
               </div>
               <div class="mt-2">
                 @error('salary')
