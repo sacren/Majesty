56845db8d27e126b146d21b45e6908a3564481e9
Jean Sacren <sakiwit@gmail.com>
2024-06-26 18:10:53 -0600

    Add validation error below input field
    Let the validation error be next to the respective input field for clear
    view.  Use bold font to let the error stand out.

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -12,22 +12,17 @@
         <h2 class="text-base font-semibold leading-7 text-gray-900">Create a new job</h2>
         <p class="mt-1 text-sm leading-6 text-gray-600">Please fill out required info.</p>
 
-        <div class="mt-5">
-          @if($errors->any())
-          <ul>
-            @foreach($errors->all() as $error)
-            <li class="text-red-500 italic">{{ $error }}</li>
-            @endforeach
-          </ul>
-          @endif
-        </div>
-
         <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
           <div class="sm:col-span-4">
             <label for="title" class="block text-sm font-medium leading-6 text-gray-900">Title</label>
             <div class="mt-2">
               <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                 <input type="text" name="title" id="title" class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Petroleum Technician">
+              </div>
+              <div class="mt-2">
+                @if($errors->has('title'))
+                <p class="text-xs text-red-500 font-bold">{{ $errors->first('title') }}</p>
+                @endif
               </div>
             </div>
           </div>
@@ -37,6 +32,11 @@
             <div class="mt-2">
               <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                 <input type="text" name="salary" id="salary" class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="$187,000.00">
+              </div>
+              <div class="mt-2">
+                @if($errors->has('salary'))
+                <p class="text-xs text-red-500 font-bold">{{ $errors->first('salary') }}</p>
+                @endif
               </div>
             </div>
           </div>
