d010b6c83f66fb92188c4f4277948f1a3e817779
Jean Sacren <sakiwit@gmail.com>
2024-06-30 19:25:59 -0600

    Add form to delete the job
    Add the form with a unique id and let it be hidden.
    
    Add the button of submit type with form attribute to the form id.

--- resources/views/jobs/edit.blade.php
+++ resources/views/jobs/edit.blade.php
@@ -58,12 +58,19 @@
       </div>
     </div>
 
-    <div class="mt-6 flex items-center justify-end gap-x-6">
-      <a href="/jobs/{{ $job->id }}" type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</a>
-      <button type="submit"
-              class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-        Update
-      </button>
+    <div class="mt-6 flex items-center justify-between gap-x-6">
+      <div class="flex items-center gap-x-3">
+        <button type="submit" form="delete-job-form" class="text-sm font-bold leading-6 text-red-500">Delete</button>
+      </div>
+      <div>
+        <a href="/jobs/{{ $job->id }}" type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</a>
+        <button type="submit"
+                class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+          Update
+        </button>
+      </div>
     </div>
+  </form>
+  <form id="delete-job-form" class="hidden">
   </form>
 </x-layout>
