4e301c84c7e534fa5bb3f19484b46398413fd7ba
Jean Sacren <sakiwit@gmail.com>
2024-07-02 23:45:12 -0600

    Organize routes
    Put Route::view() together and let Route::resource() by itself.

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
 
 Route::view('/', 'home');
+Route::view('/contact', 'contact');
 
 Route::resource('jobs', JobController::class);
-
-Route::view('/contact', 'contact');
