481488c1e764fd965d2d9c943c20e00ebb5848f6
Jean Sacren <sakiwit@gmail.com>
2024-07-04 20:19:40 -0600

    Add view component for form submit button
    Build a form submit button out of a view component so that the button
    could be used for a different form in the future.

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -35,10 +35,7 @@
 
     <div class="mt-6 flex items-center justify-end gap-x-6">
       <a href="/jobs" type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</a>
-      <button type="submit"
-              class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-        Save
-      </button>
+      <x-form-button>Save</x-form-button>
     </div>
   </form>
 </x-layout>
