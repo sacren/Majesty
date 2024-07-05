3d2ab4700659f65333347f9c3a013343b09f9700
Jean Sacren <sakiwit@gmail.com>
2024-07-05 15:07:02 -0600

    Add authentication route
    With view and controller built, add authentication route to register a
    user by the URL of /register.

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisteredUserController;
 
 Route::view('/', 'home');
 Route::view('/contact', 'contact');
 
 Route::resource('jobs', JobController::class);
+
+// Authentication Routes...
+Route::get('/register', [RegisteredUserController::class, 'create']);
