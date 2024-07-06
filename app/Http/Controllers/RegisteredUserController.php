e5f8b166d860154c8e578bb79b2e966b9f82e220
Jean Sacren <sakiwit@gmail.com>
2024-07-06 09:14:22 -0600

    Add store action for RegisteredUserController
    When the user registers by clicking Register button, the controller
    executes store action.  This invokes a list of actions.
    
    1) Validate the input
    2) Populate user database
    3) Let the user log in
    4) Redirect to certain path

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -2,10 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
 class RegisteredUserController extends Controller
 {
     public function create()
     {
         return view('auth.register');
+    }
+
+    public function store()
+    {
+        $attributes = request()->validate([
+            'first_name' => ['required', 'max:255'],
+            'last_name' => ['required', 'max:255'],
+            'email' => ['required', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'confirmed', 'min:8'],
+        ]);
+
+        $user = User::create($attributes);
+
+        Auth::login($user);
+
+        return redirect('/')->with('success', 'Your account has been created!');
     }
 }
