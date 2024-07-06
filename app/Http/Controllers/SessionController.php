0a271a08ccd717b1b4892b7033204022170224c7
Jean Sacren <sakiwit@gmail.com>
2024-07-06 14:16:46 -0600

    Add store() method for login validation
    This is the step-by-step approach for login validation.
    
    1) Validate
    2) Attempt to log in the user
    3) Regenerate the session token
    4) Redirect
    
    Particularly validate user login by checking email and password.  Use
    ValidationException if that fails.

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -2,10 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Validation\ValidationException;
+
 class SessionController extends Controller
 {
     public function create()
     {
         return view('auth.login');
+    }
+
+    public function store()
+    {
+        $attributes = request()->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (!auth()->attempt($attributes)) {
+            throw ValidationException::withMessages([
+                'email' => 'Your provided credentials are not correct'
+            ]);
+        }
+
+        request()->session()->regenerate();
+
+        return redirect('/')->with('success', 'You are now logged in!');
     }
 }
