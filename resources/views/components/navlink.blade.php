dbfec5acaed86bb596c7af8b967ee371f29d564e
Jean Sacren <sakiwit@gmail.com>
2024-07-08 18:09:42 -0600

    Revert "Use Blade directives in lieu of PHP"
    This reverts commit 69c138f05fb5e815335a552a6b5d6692e521aac5.

--- resources/views/components/navlink.blade.php
+++ resources/views/components/navlink.blade.php
@@ -3,8 +3,8 @@
     'type',
 ])
 
-@if ( $type === 'a')
+<?php if ( $type === 'a') { ?>
     <a class="{{ $active ? 'bg-gray-900 text-white':'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium" aria-current="{{ $active ? 'page':'false' }}" {{ $attributes }}>{{ $slot }}</a>
-@else
+<?php } else { ?>
     <button class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="false" {{ $attributes }}>{{ $slot }}</button>
-@endif
+<?php } ?>
