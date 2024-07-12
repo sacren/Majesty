5d77ca68dc9a18a5954d8a4ccc41810feb6724c0
Jean Sacren <sakiwit@gmail.com>
2024-07-12 17:23:01 -0600

    Restore all routes of jobs
    Route::resource() has a downside of obscuring all the routes of jobs.
    Let routes of jobs be visible by using Route::get() and its friends.

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,14 @@ use App\Http\Controllers\SessionController;
 Route::view('/', 'home');
 Route::view('/contact', 'contact');
 
-Route::resource('jobs', JobController::class);
+// Jobs Routes...
+Route::get('/jobs', [JobController::class, 'index']);
+Route::post('/jobs', [JobController::class, 'store']);
+Route::get('/jobs/create', [JobController::class, 'create']);
+Route::get('/jobs/{job}', [JobController::class, 'show']);
+Route::get('/jobs/{job}/edit', [JobController::class, 'edit']);
+Route::patch('/jobs/{job}', [JobController::class, 'update']);
+Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
 
 // Authentication Routes...
 Route::get('/register', [RegisteredUserController::class, 'create']);
@@ -17,5 +24,4 @@ Route::post('/register', [RegisteredUserController::class, 'store']);
 // Login Routes...
 Route::get('/login', [SessionController::class, 'create']);
 Route::post('/login', [SessionController::class, 'store']);
-
 Route::post('/logout', [SessionController::class, 'destroy']);
