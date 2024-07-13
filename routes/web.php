da34b7229f9697f31c3b1aec82c2518d1f97fdcf
Jean Sacren <sakiwit@gmail.com>
2024-07-12 22:09:46 -0600

    Add job edit authorization at routes level
    Gate::authorize() was done through jobs edit or jobs show.  Now move
    this authorization at routes level through the use of middleware and
    chain ->middleware() and ->can().

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,11 @@ Route::get('/jobs', [JobController::class, 'index']);
 Route::post('/jobs', [JobController::class, 'store']);
 Route::get('/jobs/create', [JobController::class, 'create']);
 Route::get('/jobs/{job}', [JobController::class, 'show']);
-Route::get('/jobs/{job}/edit', [JobController::class, 'edit']);
+
+Route::get('/jobs/{job}/edit', [JobController::class, 'edit'])
+    ->middleware('auth')
+    ->can('edit-job', 'job');
+
 Route::patch('/jobs/{job}', [JobController::class, 'update']);
 Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
 
@@ -22,6 +26,6 @@ Route::get('/register', [RegisteredUserController::class, 'create']);
 Route::post('/register', [RegisteredUserController::class, 'store']);
 
 // Login Routes...
-Route::get('/login', [SessionController::class, 'create']);
+Route::get('/login', [SessionController::class, 'create'])->name('login');
 Route::post('/login', [SessionController::class, 'store']);
 Route::post('/logout', [SessionController::class, 'destroy']);
