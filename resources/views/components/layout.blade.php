9c5ed1de5d547a882c01081e79a7f5c09b0aa643
Jean Sacren <sakiwit@gmail.com>
2024-07-24 15:18:27 -0600

    Remove Tailwind CDN
    Tailwind is installed locally and vite is serving it.  Tailwind CDN is
    for temporary use and now we can clean it up for good.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ $title ?? 'Page' }}</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    <script src="https://cdn.tailwindcss.com"></script>
   </head>
   <body class="h-full">
     <!--
