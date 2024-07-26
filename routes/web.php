dfd0b9488ea79b9b0a461564ef768ab58c21d278
Jean Sacren <sakiwit@gmail.com>
2024-07-26 16:59:39 -0600

    Use Inertia::render() static method
    Inertia::render() static method is preferred over inertia() helper.

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,19 @@
 <?php
 
-use Illuminate\Support\Facades\Mail;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\SessionController;
 use App\Jobs\TranslateJob;
 use App\Mail\JobPosted;
 use App\Models\Job;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::view('/', 'home');
 Route::view('/contact', 'contact');
 Route::get('/welcome', function () {
-    return inertia('welcome');
+    return Inertia::render('welcome');
 });
 
 Route::get('dispatched', function () {
