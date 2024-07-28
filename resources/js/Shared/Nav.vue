0bfd0531e45783a65beffd4c1553bd285c62be26
Jean Sacren <sakiwit@gmail.com>
2024-07-28 14:06:10 -0600

    Add shared component of Nav
    Inertia lets common components be shared across multiple vue pages and
    they are stored in Shared.
    
    Add Nav component so that it might be shared among Home, Users and
    Settings vue pages.

---
+++ resources/js/Shared/Nav.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav>
+    <ul>
+      <li><Link href="/spa">Home</Link></li>
+      <li><Link href="/spa/users">Users</Link></li>
+      <li><Link href="/spa/settings">Settings</Link></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+  import { Link } from '@inertiajs/inertia-vue3';
+
+  export default {
+    components: {
+      Link,
+    },
+  };
+</script>
