7165d3db32ab1b370a6e3a9f287767bae75fae6e
Jean Sacren <sakiwit@gmail.com>
2024-07-30 09:47:58 -0600

    Add a link to update time
    Use Link component from inertiajs to add a link to Home page.  By
    reloading the page we can update the time from the server.

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -6,15 +6,21 @@
   <Nav />
     <div>
       <p>The current time is {{ time }}</p>
+
+      <Link href="/spa" class="text-blue-500 hover:underline">
+      Update Time
+      </Link>
     </div>
 </template>
 
 <script>
   import Nav from '../Shared/Nav.vue';
+  import { Link } from '@inertiajs/inertia-vue3';
 
   export default {
     components: {
       Nav,
+      Link,
     },
     props: {
       time: String,
