f3f271b6dcec5b75beb141bebfe5701ce0355d0b
Jean Sacren <sakiwit@gmail.com>
2024-08-05 11:59:51 -0600

    Add test of 'it belongs to an employer'
    Add the test that the job created belongs to the employer.  Follow AAA
    procedure of Arrange, Act and Assert.  Particular to this test, combine
    Act and Assert due to the simple nature of the test.
    
    Use the alias of it in lieu of test to construct the test.
    
    By default pest is used for feature test only.  Add unit test to pest.
    Without the unit test added, it will throw this exception.
    
       FAILED  Tests\Unit\JobTest > it belongs to an employer
       RuntimeException
     A facade root has not been set.
    
    While there, add RefreshDatabase so that the test is run in pristine
    state.

--- tests/Unit/JobTest.php
+++ tests/Unit/JobTest.php
@@ -1,5 +1,15 @@
 <?php
 
-test('example', function () {
-    expect(true)->toBeTrue();
+use App\Models\Employer;
+use App\Models\Job;
+
+it('belongs to an employer', function () {
+    // Arrange
+    $employer = Employer::factory()->create();
+    $job = Job::factory()->create([
+        'employer_id' => $employer->id,
+    ]);
+
+    // Act and Assert
+    expect($job->employer->is($employer))->toBeTrue();
 });
