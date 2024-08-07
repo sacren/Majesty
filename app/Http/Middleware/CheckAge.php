bbeb29326258b4decd970b2dc82061a969c929ec
Jean Sacren <sakiwit@gmail.com>
2024-08-07 14:15:46 -0600

    Add CheckAge middleware
    Use artisan tool to create CheckAge middleware.
    
    $ sail artisan make:middleware -- CheckAge
    
       INFO  Middleware [app/Http/Middleware/CheckAge.php] created
       successfully.
    
    This middleware is used to check the age.  Set the minimum age to 13 and
    page status to 403 Forbidden with age under 13.

---
+++ app/Http/Middleware/CheckAge.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckAge
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $age = $request->age;
+
+        if ($age < 13) {
+            return response('You must be at least 13 years old', 403);
+        }
+
+        return $next($request);
+    }
+}
