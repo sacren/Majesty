7ba8cb58071dc2bed3d2ac9753ffc3427dd324d1
Jean Sacren <sakiwit@gmail.com>
2024-08-07 18:06:39 -0600

    Use global component for Link
    Importing Link for anchor tag in each vue page was redundant.  It is
    better to import Link component in main Javascript file, letting each
    vue page enjoy global Link component.
    
    It is also good to solely rely on the new @inertiajs/vue3 package.

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -16,12 +16,10 @@
 
 <script>
   import Layout from '../Shared/Layout.vue';
-  import { Link } from '@inertiajs/inertia-vue3';
 
   export default {
     components: {
       Layout,
-      Link,
     },
   };
 </script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,6 @@
 import './bootstrap';
 import { createApp, h } from 'vue';
-import { createInertiaApp } from '@inertiajs/vue3';
+import { createInertiaApp, Link } from '@inertiajs/vue3';
 
 createInertiaApp({
     progress: {
@@ -16,6 +16,7 @@ createInertiaApp({
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
+            .component('Link', Link)
             .mount(el);
     },
 });
