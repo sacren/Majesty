76dab4ae0d5c2e3850a10883c2aaf3a498a8fa58
Jean Sacren <sakiwit@gmail.com>
2025-10-26 01:54:30 -0600

    Remove vite-plugin-wayfinder in vite.config.ts
    Clean up fake vite-plugin-wayfinder and run sail npm install next.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
-import { wayfinder } from '@laravel/vite-plugin-wayfinder';
-import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
@@ -12,9 +11,6 @@ export default defineConfig({
             refresh: true,
         }),
         tailwindcss(),
-        wayfinder({
-            formVariants: true,
-        }),
         vue({
             template: {
                 transformAssetUrls: {
