ab9d28b558092eac8181fa121f67cac0376fe32d
Jean Sacren <sakiwit@gmail.com>
2025-11-01 16:44:44 -0600

    Revert "Remove the global declaration"
    This reverts commit 7db73ab3dfa94d26262c963b7151b741ac323ca0.

--- resources/js/types/ziggy.d.ts
+++ resources/js/types/ziggy.d.ts
@@ -1,4 +1,8 @@
-import type { route } from 'ziggy-js';
+import { route } from 'ziggy-js';
+
+declare global {
+    let route: typeof route;
+}
 
 declare module 'vue' {
     interface ComponentCustomProperties {
